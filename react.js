547cbdaf43f831e53ecd50b1ffd0f2f6f1946352
Craig Morrison <public@craigmorrison.co.uk>
2021-05-22 17:04:31 +0100

    Add React support

---
+++ react.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended'
+  ],
+  plugins: ['react', 'react-hooks', 'jsx-a11y'],
+  rules: {}
+};
