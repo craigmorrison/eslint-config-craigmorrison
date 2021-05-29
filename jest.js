f23d900b5ea186f0cf4e19cc2222fb8487ed6bed
Craig Morrison <868089+craigmorrison@users.noreply.github.com>
2021-05-29 23:33:19 +0100

    Update rules

--- jest.js
+++ jest.js
@@ -10,5 +10,9 @@ module.exports = {
       version: 26
     }
   },
-  extends: ['plugin:jest/all']
+  extends: ['plugin:jest/all'],
+  rules: {
+    'jest/lowercase-name': ['error', { ignoreTopLevelDescribe: true }],
+    'jest/prefer-expect-assertions': 'off'
+  }
 };
